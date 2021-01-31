b666b7e4c3230c380482f1b690844352eea4b44c
vijay5687 <58792348+vijay5687@users.noreply.github.com>
2021-01-31 12:24:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'vinay kumar',                   # Type in your name
   author_email = 'kvinni17@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/vijay5687/edcutil',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/vijay5687/edcutil/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/vijay5687/edcutil/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
