f35c5150b8c20e1640e14cb2b290ed1e2a3d3451
vijay5687 <58792348+vijay5687@users.noreply.github.com>
2021-01-31 12:39:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setup(
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
-          'json',
-          'os',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
